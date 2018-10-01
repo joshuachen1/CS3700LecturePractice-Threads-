eafa79dbb57d179a2ae09c76fb9d28ffcf8c2550
Joshua Chen <joshuachen1@cpp.edu>
2018-10-01 14:14:53 -0700

    second commit (change sleep)

--- SleepMessages.java
+++ SleepMessages.java
@@ -12,8 +12,8 @@ public class SleepMessages {
         for (int i = 0;
              i < importantInfo.length;
              i++) {
-            //Pause for 4 seconds
-            Thread.sleep(4000);
+            //Pause for 2 seconds
+            Thread.sleep(2000);
             //Print a message
             System.out.println(importantInfo[i]);
         }
